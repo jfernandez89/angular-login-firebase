d3a91f41cf187ec45f99f833a26321dd653ad698
javier Fernandez <jfernandez.developer@gmail.com>
2020-02-19 19:37:21 +0100

    Added a form variable to the template html and some validations

--- angular-login/src/app/pages/registro/registro.component.ts
+++ angular-login/src/app/pages/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from '../../models/usuario.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -17,8 +18,15 @@ export class RegistroComponent implements OnInit {
     this.usuario.email = "jfernandez89@gmail.com"
   }
 
-  getData() {
-    console.log("Formulario enviado");
-    console.log(this.usuario);
+  // Pasamos el formulario como parametro desde la template html
+  onSubmit(form: NgForm) {
+
+    if(form.invalid){
+      return;
+    } else {
+      console.log("Formulario enviado");
+      console.log(this.usuario);
+      console.log(form);
+    }
   }
 }
